a76e7446d4f671bf8eeec22d5d865785a96bf7e3
OrayKT <oraykt@gmail.com>
2024-11-09 22:57:04 +0100

    Lodash and generateMetaData

--- src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import { OrgControl } from "./_components/org-control";
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+  return {
+    title: startCase(orgSlug || "organization"),
+  }
+}
 
 interface OrganizationIdLayoutProps {
   children: React.ReactNode;
